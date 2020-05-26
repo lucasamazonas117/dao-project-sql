0b6fa47dada73405856cb8c25d424e903b3e090e
Gian Lucas Amazonas <lucasamazonas117@gmail.com>
2020-05-26 01:18:27 -0300

    Created: Program, Department

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Project SQL
+		
+		Department d = new Department(117, "Books");
+		System.out.println(d);
+
+	}
+
+}
