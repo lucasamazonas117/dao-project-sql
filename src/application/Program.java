470d00e83917bcc010f5cdf621aa91311245fb46
Gian Lucas Amazonas <lucasamazonas117@gmail.com>
2020-05-26 01:28:04 -0300

    Created: Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,8 @@ public class Program {
 		// Project SQL
 		
 		Department d = new Department(117, "Books");
-		System.out.println(d);
+		Seller s = new Seller(21, "Gian", "lucasamazonas@gmail.com", new Date(), 5000.0, d);
+		System.out.println(s);
 
 	}
 
